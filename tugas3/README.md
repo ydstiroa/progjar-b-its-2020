c17b395b2126f5893b733aec9174858789ed85aa
ydstiroa <yudhistiroadi@rocketmail.com>
2020-03-09 16:21:59 +0700

    tugas3

---
+++ tugas3/README.md
@@ -0,0 +1,16 @@
+# Tugas 2
+
+* Output pada terminal dari menjalankan program Multithreading
+![Capture Udp_simple](Capture/output_program.PNG)
+
+* Bukti foto berhasil terdownload
+
+![Capture Udp_simple](Capture/terdownload.PNG)
+
+* Berikut adalah hasil fotonya
+Foto pertama
+![Capture Udp_simple](Capture/Foto Ke-1.PNG)
+Foto Kedua
+![Capture Udp_simple](Capture/Foto Ke-2.PNG)
+Foto Ketiga
+![Capture Udp_simple](Capture/Foto Ke-3.PNG)
---
+++ tugas3/tugas3.py
@@ -0,0 +1,43 @@
+import logging
+import requests
+import threading
+import datetime
+import os
+
+def download_gambar(url=None,nama=None):
+    if (url is None):
+        return False
+    ff = requests.get(url)
+    tipe = dict()
+    tipe['image/png']='png'
+    tipe['image/jpg']='jpg'
+    tipe['image/jpeg']='jpg'
+    
+    currentDT = datetime.datetime.now()
+    currentDT = currentDT.strftime("%Y-%m-%d %H:%M:%S")
+
+    content_type = ff.headers['Content-Type']
+    logging.warning(content_type)
+    if (content_type in list(tipe.keys())):
+        namafile = nama
+        ekstensi = tipe[content_type]
+        logging.warning(f"\n Download {namafile}.{ekstensi},\n Date = {currentDT} ")
+        fp = open(f"{namafile}.{ekstensi}","wb")
+        fp.write(ff.content)
+        fp.close()
+    else:
+        return False
+
+
+if __name__=='__main__':
+
+    threads = []
+
+    x = threading.Thread(target=download_gambar, args=('https://asset.kompas.com/crops/AaItk5N9tIU_oFAfrf_kCyFL8YE=/0x0:0x0/750x500/data/photo/2020/03/09/5e65b4908df04.jpg','Foto Ke-1',))
+    threads.append(x)
+    x = threading.Thread(target=download_gambar, args=('https://asset.kompas.com/crops/3v1kOS1k0kFjbzPDSIqTLtXEPbc=/18x0:734x477/750x500/data/photo/2020/03/09/5e65613d60411.jpg','Foto Ke-2',))
+    threads.append(x)
+    x = threading.Thread(target=download_gambar, args=('https://asset.kompas.com/crops/l-zUYoLCXYyKqMaAVNz9SexPXsg=/0x0:864x576/750x500/data/photo/2018/11/16/2037804675.jpg','Foto Ke-3',))
+    threads.append(x)
+    for i in threads:
+        i.start()
